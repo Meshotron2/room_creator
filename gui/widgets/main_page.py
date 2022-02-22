3099f912f8013f21bfb9a995a66584fb8f568967
Joao-Felisberto <joaofelisberto@ua.pt>
2022-02-22 15:20:54 +0000

    [CLUSTER-76] Integrated status GUI

--- gui/widgets/main_page.py
+++ gui/widgets/main_page.py
@@ -2,6 +2,7 @@ from PySide6 import QtWidgets, QtCore
 from PySide6.QtWidgets import QSizePolicy
 
 from widgets.creation_wizard import WizardWidget
+from widgets.status_page import StatusPageWidget
 
 
 class MainWidget(QtWidgets.QWidget):
@@ -37,8 +38,17 @@ class MainWidget(QtWidgets.QWidget):
 
         self.content_layout.addWidget(self.btn_start_wizard)
 
+        self.monitor_btn = QtWidgets.QPushButton("Check the status of a running process")
+        self.server_input = QtWidgets.QLineEdit()
+        self.server_input.setPlaceholderText("Server ip and port")
+
+        self.content_layout.addItem(QtWidgets.QSpacerItem(20, 10, QSizePolicy.Minimum, QSizePolicy.Expanding))
+        self.content_layout.addWidget(self.server_input)
+        self.content_layout.addWidget(self.monitor_btn)
+
         self.btn_select_file.clicked.connect(self.select_file)
         self.btn_start_wizard.clicked.connect(self.open_wizard)
+        self.monitor_btn.clicked.connect(self.open_status_monitor)
 
     @QtCore.Slot()
     def select_file(self):
@@ -49,4 +59,10 @@ class MainWidget(QtWidgets.QWidget):
     @QtCore.Slot()
     def open_wizard(self):
         self.open_window = WizardWidget()
+        self.open_window.show()
+
+    @QtCore.Slot()
+    def open_status_monitor(self):
+        server = self.server_input.text()
+        self.open_window = StatusPageWidget(server)
         self.open_window.show()
